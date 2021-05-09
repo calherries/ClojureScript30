53da67fe674286f431692398218da6ea8018258c
Callum Herries <hi@callumherries.com>
2021-05-09 14:22:39 +0100

    Add content for drumkit

---
+++ 01-drumkit/src/app/core.cljs
@@ -0,0 +1,12 @@
+(ns app.core)
+
+;; start is called by init and after code reloading finishes
+(defn ^:dev/after-load start []
+  (js/console.log "start"))
+
+(defn init []
+  ;; init is called ONCE when the page loads
+  ;; this is called in the index.html and must be exported
+  ;; so it is available even in :advanced release builds
+  (js/console.log "init")
+  (start))
