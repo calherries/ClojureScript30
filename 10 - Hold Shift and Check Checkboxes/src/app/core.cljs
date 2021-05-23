baf2f706886945c6ab27835f112f4ae67fad7690
Callum Herries <hi@callumherries.com>
2021-05-23 15:21:33 +0200

    Complete 10

--- 10 - Hold Shift and Check Checkboxes/src/app/core.cljs
+++ 10 - Hold Shift and Check Checkboxes/src/app/core.cljs
@@ -1,4 +1,5 @@
-(ns app.core)
+(ns app.core
+  (:require [cljs-bean.core :refer [bean ->clj ->js]]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Dev utils
@@ -22,7 +23,14 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Start
 
-(def checkboxes (array-seq (js/document.querySelectorAll ".inbox input[type=\"checkbox\"]")))
+(js/document.querySelectorAll ".inbox input[type=\"checkbox\"]")
+(cljs (js/document.querySelector ".inbox input[type=\"checkbox\"]"))
+(bean (js/document.querySelector ".inbox input[type=\"checkbox\"]") :recursive :true)
+
+(->clj #js {:a 1, :b 2})
+
+(def checkboxes
+  (array-seq (js/document.querySelectorAll ".inbox input[type=\"checkbox\"]")))
 
 (def last-checked (atom nil))
 
@@ -32,21 +40,20 @@
 
 (defn handle-check [e]
   (this-as this
-   (when (and (-> e .-shiftKey)
-              (-> this .-checked)
-              @last-checked)
-     (let [in-between-checkboxes (->> checkboxes
-                                      (keep-indexed (fn [idx item]
-                                                      (when (or (identical? this item)
-                                                                (identical? @last-checked item))
-                                                        idx)))
-                                      (apply range)
-                                      (filter-by-index checkboxes))]
-       (run! #(set! (.-checked %) true) in-between-checkboxes)))
-   (reset! last-checked this)))
+           (when (and (-> e .-shiftKey)
+                      (-> this .-checked)
+                      @last-checked)
+             (let [in-between-checkboxes (->> checkboxes
+                                              (keep-indexed (fn [idx item]
+                                                              (when (or (identical? this item)
+                                                                        (identical? @last-checked item))
+                                                                idx)))
+                                              (apply range)
+                                              (filter-by-index checkboxes))]
+               (run! #(set! (.-checked %) true) in-between-checkboxes)))
+           (reset! last-checked this)))
+
+;; start
 
 (doseq [checkbox checkboxes]
   (.addEventListener checkbox "click" handle-check))
-
-(defn init []
-  (js/console.log "init"))
