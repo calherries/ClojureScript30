aae7716cd378c5ee0cb9b1bf33dc715a3279faf4
Callum Herries <hi@callumherries.com>
2021-06-06 16:18:29 +0200

    Complete 20

--- 20 - Speech Detection/src/app/core.cljs
+++ 20 - Speech Detection/src/app/core.cljs
@@ -32,6 +32,7 @@
 (def recognition (js/SpeechRecognition.))
 
 (set! (.-interimResults recognition) true)
+
 (set! (.-lang recognition) "en-US")
 
 (def p (js/document.createElement "p"))
@@ -40,17 +41,20 @@
 
 (.appendChild words p)
 
-(.addEventListener recognition "result"
-                   (fn [e]
-                     (js/console.log e)
-                     (let [transcript (->> e
-                                           .-results
-                                           array-seq
-                                           (map (fn [result]
-                                                  (.-transcript (aget result 0))))
-                                           (apply str))]
-                       (js/console.log (.-results e)))))
+(defn handle-results [e]
+  (let [transcript (->> e
+                        .-results
+                        array-seq
+                        (map (fn [results]
+                               (.-transcript (aget results 0))))
+                        (apply str))
+        poop-script (clojure.string/replace transcript #"(?i)poop|poo|shit|dump" "💩")]
+    (set! (.-textContent p) poop-script)
+    (when (-> e .-results (aget 0) .-isFinal)
+      (let [p (js/document.createElement "p")]
+        (.appendChild words p)))))
 
+(.addEventListener recognition "result" handle-results)
 
 (.addEventListener recognition "end" (.-start recognition))
 
